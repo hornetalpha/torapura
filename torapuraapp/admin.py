ab0bb6b9b3537505e0be9f235c8c6ac0b24461e3
d-takahashi <150633274+hornetalpha@users.noreply.github.com>
2024-11-19 12:26:33 +0900

    Update admin.py

--- torapuraapp/admin.py
+++ torapuraapp/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.formats import base_formats
 from .models import Post
+from .models import Plan
+
+class PlanResource(resources.ModelResource):
+    # Modelに対するdjango-import-exportの設定
+    class Meta:
+        model = Plan
+        fields = ('id', 'place', 'done', 'person', 'day', 'plan_name', 'plan_detail', 'typeno')
+        import_id_fields = ['id']
+
+class PlanAdmin(ImportExportModelAdmin):
+    # ImportExportModelAdminを利用するようにする
+    list_display = ('place', 'done', 'person', 'day', 'plan_name', 'plan_detail', 'typeno')
+    # django-import-exportsの設定
+    resource_class = PlanResource
+    formats = [base_formats.CSV]
 
 admin.site.register(Post)
+admin.site.register(Plan, PlanAdmin)
