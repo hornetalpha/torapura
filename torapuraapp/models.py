a11a6d300b7d13fdc5c03daed534af1ea74588db
tsukuiyuzuki <150633374+tsukuiyuzuki@users.noreply.github.com>
2024-11-21 12:19:59 +0900

    Update models.py

--- torapuraapp/models.py
+++ torapuraapp/models.py
@@ -4,6 +4,15 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+    date = models.DateField(default=timezone.now)  # 手動入力または自動入力可能な日付
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField("タイトル", max_length=200)
     content = models.TextField("本文")
