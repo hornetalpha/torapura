1b68af0ee1f5f485f9f3bec86664a9d771033f07
tsukuiyuzuki <150633374+tsukuiyuzuki@users.noreply.github.com>
2024-11-21 12:27:48 +0900

    Update urls.py

--- torapuraapp/urls.py
+++ torapuraapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .views import GalleryView
 
 # URLconfのURLパターンを逆引きできるようにアプリ名を登録
 app_name = "torapuraapp"
@@ -11,10 +12,11 @@ urlpatterns = [
     # viewsモジュールのIndexViewを実行
     # URLパターン名は'index'
     path("", views.IndexView.as_view(), name="index"),
-    path("gallery", views.GalleryView.as_view(), name="gallery"),
+    path("gallery", views.Post_List_View.as_view(), name="gallery"),
     path("contact", views.ContactView.as_view(), name="contact"),
     path("contact_user", views.ContactUserView.as_view(), name="contact_user"),
     path("contact_company", views.ContactCompanyView.as_view(), name="contact_company"),
+    path("", GalleryView.as_view(), name="post_list"),
     path("plan", views.PlanView.as_view(), name="plan"),
-    path('plan/result/', views.PlanResultView.as_view(), name='plan_result'),
+    path("plan/result/", views.PlanResultView.as_view(), name="plan_result"),
 ]
